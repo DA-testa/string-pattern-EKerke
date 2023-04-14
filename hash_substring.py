37a0303c70c108ac1b004ac528992fdbeec76fba
221RDB044 <124919320+EKerke@users.noreply.github.com>
2023-04-14 21:41:55 +0000

    10

--- hash_substring.py
+++ hash_substring.py
@@ -4,15 +4,15 @@ import sys
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    in_type = input().rstrip().lower()
+    in_type = input().strip().lower()
     if in_type == 'f':
-        path = input().rstrip()
+        path = input().strip()
         with open(path, 'r') as f:
-            pattern = f.readline().rstrip()
-            text = f.readline().rstrip()
+            pattern = f.readline().strip()
+            text = f.readline().strip()
     else:
-        pattern = input().rstrip()
-        text = input().rstrip()
+        pattern = input().strip()
+        text = input().strip()
 
     
     # after input type choice
@@ -42,18 +42,20 @@ def get_occurrences(pattern, text):
     for i in range(m):
         h_pattern = (h_pattern * p + ord(pattern[i])) % q
 
-    h_text = [0] * (n - m + 1)
-    h_text[0] = 0
+    h_text = 0
     for i in range(m):
         h_text[0] = (h_text[0] * p + ord(pattern[i])) % q
-    for i in range(1, n - m + 1):
-        h_text[i] = ((h_text[i-1] - ord(text[i-1]) * pow(p, m-1, q)) * p + ord(text[i+m-1])) % q
 
-    position = []
-    for i in range(n - m +1):
-        if h_text[i] == h_pattern:
-            if text[i:i+m] == pattern:
-                position.append(i)
+    position = [] 
+    if h_text == h_pattern and text[:m] == pattern:
+        position.append(0)
+
+    p_pow = pow(p, m-1, q) 
+    for i in range(1, n - m + 1): 
+        h_text = (h_text - ord(text[i-1]) * p_pow) % q 
+        h_text = (h_text * p + ord(text[i+m-1])) % q 
+        if h_text == h_pattern and text[i:i+m] == pattern: 
+            position.append(i) 
 
     # and return an iterable variable
     return position
