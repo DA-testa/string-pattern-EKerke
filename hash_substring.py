5a06aa6e50ce8aeb65059f62a5815d92e91c943e
221RDB044 <124919320+EKerke@users.noreply.github.com>
2023-04-14 20:37:50 +0000

    2

--- hash_substring.py
+++ hash_substring.py
@@ -3,8 +3,6 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    input type will follow
-    in_type = input().rstrip().lower()
     if in_type == 'f':
         path = input().rstrip()
         with open(path, 'r') as f:
@@ -23,7 +21,7 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (pattern, text)
+    return pattern, text
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
