89a547bf421eeb6bb2c8288d9eea4a8a4e90a565
221RDB044 <124919320+EKerke@users.noreply.github.com>
2023-04-14 22:13:52 +0000

    211

--- hash_substring.py
+++ hash_substring.py
@@ -27,7 +27,8 @@ def read_input():
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
-    print(' '.join(map(str, output))) 
+    #print(' '.join(map(str, output))) 
+    print(output)
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
